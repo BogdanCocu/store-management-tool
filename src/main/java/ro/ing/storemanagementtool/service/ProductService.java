8595fe5440e10d9b8f198fa02698519e8ef1cb81
BogdanCocu <bcocu97@gmail.com>
2023-03-20 14:47:06 +0200

    Added convert methods Product and ProductDto

--- src/main/java/ro/ing/storemanagementtool/service/ProductService.java
+++ src/main/java/ro/ing/storemanagementtool/service/ProductService.java
@@ -2,6 +2,8 @@ package ro.ing.storemanagementtool.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ro.ing.storemanagementtool.domain.Product;
+import ro.ing.storemanagementtool.dto.ProductDto;
 import ro.ing.storemanagementtool.repository.ProductRepository;
 
 @Service
@@ -9,4 +11,20 @@ public class ProductService {
 
     @Autowired
     private ProductRepository productRepository;
+
+    public ProductDto toDto(Product product) {
+        ProductDto productDto = new ProductDto();
+        productDto.setProductName(product.getProductName());
+        productDto.setDescription(product.getDescription());
+        productDto.setPrice(product.getPrice());
+        return productDto;
+    }
+
+    public Product toEntity(ProductDto productDto) {
+        Product product = new Product();
+        product.setProductName(productDto.getProductName());
+        product.setDescription(productDto.getDescription());
+        product.setPrice(productDto.getPrice());
+        return product;
+    }
 }
