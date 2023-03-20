1b0496e78abcf5acbefd8f07ba02d9477640e04d
BogdanCocu <bcocu97@gmail.com>
2023-03-20 14:01:10 +0200

    Product entity created

---
+++ src/main/java/ro/ing/storemanagementtool/domain/Product.java
@@ -0,0 +1,55 @@
+package ro.ing.storemanagementtool.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    private long id;
+    private String productName;
+    private long price;
+    private String description;
+
+    public Product() {
+    }
+
+    public Product(String productName, long price, String description) {
+        this.productName = productName;
+        this.price = price;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
