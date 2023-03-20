5c9a8ed527c517952c078a67af3ef7a656173b04
BogdanCocu <bcocu97@gmail.com>
2023-03-20 14:13:25 +0200

    Product Repository added

--- src/main/java/ro/ing/storemanagementtool/domain/Product.java
+++ src/main/java/ro/ing/storemanagementtool/domain/Product.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 public class Product {
 
     @Id
-    private long id;
+    private Long id;
     private String productName;
     private long price;
     private String description;
@@ -21,11 +21,11 @@ public class Product {
         this.description = description;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
---
+++ src/main/java/ro/ing/storemanagementtool/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package ro.ing.storemanagementtool.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ro.ing.storemanagementtool.domain.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    
+}
