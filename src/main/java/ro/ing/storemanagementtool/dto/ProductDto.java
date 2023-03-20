b0913fd259487d9373273b93b3a17b47075fe1a1
BogdanCocu <bcocu97@gmail.com>
2023-03-20 14:31:26 +0200

    ProductDto added

---
+++ src/main/java/ro/ing/storemanagementtool/dto/ProductDto.java
@@ -0,0 +1,31 @@
+package ro.ing.storemanagementtool.dto;
+
+public class ProductDto {
+    private String productName;
+    private long price;
+    private String description;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
